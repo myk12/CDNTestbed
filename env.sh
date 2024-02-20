0cbd198376e595dfd188c56d3cda23c8d925460e
mayuke <ykma22@m.fudan.edu.cn>
2024-02-20 06:21:43 -0600

    env: update environment setup script

--- env.sh
+++ env.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# 安装 Zsh
+sudo apt update
+sudo apt install zsh -y
+
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ~/.zshrc ~/.zshrc.bak
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+echo "alias ll='ls -alF'" >> ~/.zshrc
+
+# Install Golang
+
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+
+echo "\$PATH=\$PATH:/usr/local/go/bin/"  >> ~/.bashrc
+source ~/.bashrc
+
+go version
+
 # Install IPFS kubo
 wget https://dist.ipfs.tech/kubo/v0.26.0/kubo_v0.26.0_linux-amd64.tar.gz
 
@@ -9,3 +31,6 @@ cd kubo
 sudo bash install.sh
 
 ipfs --version
+
+
+rm -rf *tar*
