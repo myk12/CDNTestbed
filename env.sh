beb7528e236aaffe9eadae2642f52f70cf50d9bf
mayuke <393810965@qq.com>
2024-02-21 18:48:52 +0800

    data: add swarm.key for private IPFS

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@
 sudo apt update
 sudo apt install zsh -y
 
-chsh -s $(which zsh)
+yes | chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ~/.zshrc ~/.zshrc.bak
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
@@ -17,8 +17,9 @@ echo "alias ll='ls -alF'" >> ~/.zshrc
 wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 
-echo "\$PATH=\$PATH:/usr/local/go/bin/"  >> ~/.bashrc
-source ~/.bashrc
+export PATH="$PATH:/usr/local/go/bin"
+echo "export PATH=\$PATH:/usr/local/go/bin/"  >> ~/.zshrc
+
 
 go version
 
