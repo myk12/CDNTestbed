95d8476516fe2cf4b39c7aac3f80338daf3b418e
mayuke <393810965@qq.com>
2024-02-21 19:02:10 +0800

    feat: modify env script to add new features
    1. ipfs init
    2. copy swarm.key to ~/.ipfs/

--- env.sh
+++ env.sh
@@ -2,7 +2,20 @@
 
 #!/bin/bash
 
-# 安装 Zsh
+# Install Golang
+
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+go version
+
+# Install IPFS kubo
+wget https://dist.ipfs.tech/kubo/v0.26.0/kubo_v0.26.0_linux-amd64.tar.gz
+tar -xvzf kubo_v0.26.0_linux-amd64.tar.gz
+cd kubo
+sudo bash install.sh
+cd ..
+
+# Install Zsh
 sudo apt update
 sudo apt install zsh -y
 
@@ -12,26 +25,19 @@ cp ~/.zshrc ~/.zshrc.bak
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 echo "alias ll='ls -alF'" >> ~/.zshrc
 
-# Install Golang
 
-wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+# Config environment
 
+## go
 export PATH="$PATH:/usr/local/go/bin"
 echo "export PATH=\$PATH:/usr/local/go/bin/"  >> ~/.zshrc
-
-
 go version
 
-# Install IPFS kubo
-wget https://dist.ipfs.tech/kubo/v0.26.0/kubo_v0.26.0_linux-amd64.tar.gz
-
-tar -xvzf kubo_v0.26.0_linux-amd64.tar.gz
-
-cd kubo
-sudo bash install.sh
-
+## ipfs
 ipfs --version
+ipfs init
+cp swarm.key ~/.ipfs/
+
 
 
 rm -rf *tar*
