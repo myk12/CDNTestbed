8b7dc722b6563f3ae3a1fe7042e935de6588c819
mayuke <393810965@qq.com>
2024-04-28 22:48:26 +0800

    varnish: fix bug with crontab

--- varnish/install-varnish.sh
+++ varnish/install-varnish.sh
@@ -3,6 +3,11 @@
 # install varnish
 sudo apt-get install varnish
 
-# varnish stat
+# Define the crontab command
 crontab_cmd="0 * * * * /bin/bash /users/gtc/CDNTestbed/varnish/varnishstat.sh >> /users/gtc/varnishstat.log 2>&1"
-crontab < (echo "${crontab_cmd}")
+
+# Add the crontab entry using command substitution and process substitution
+echo "${crontab_cmd}" | crontab -
+
+# Alternatively, you can use the here-string (<<<) operator
+# crontab <<< "${crontab_cmd}"
