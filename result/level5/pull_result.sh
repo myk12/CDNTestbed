1380721396f1d95403ecd57252f03f563dde806b
mayuke <393810965@qq.com>
2024-05-22 18:56:54 +0800

    feat: new progress of socialCDN

--- result/level5/pull_result.sh
+++ result/level5/pull_result.sh
@@ -5,15 +5,15 @@ node4_fullname="ssh gtc@c220g5-110528.wisc.cloudlab.us"
 node5_fullname="ssh gtc@amd021.utah.cloudlab.us"
 node6_fullname="ssh gtc@amd019.utah.cloudlab.us"
 
-node3_name=$(echo ${node3_fullname} | awk -F ' ' '{print$1}')
+node3_name=$(echo ${node3_fullname} | awk -F ' ' '{print$2}')
 scp $node3_name:/users/gtc/varnishstat.log ./varnishstat-node3.log
 
-node4_name=$(echo ${node4_fullname} | awk -F ' ' '{print$1}')
+node4_name=$(echo ${node4_fullname} | awk -F ' ' '{print$2}')
 scp $node4_name:/users/gtc/varnishstat.log ./varnishstat-node4.log
 
-node5_name=$(echo ${node5_fullname} | awk -F ' ' '{print$1}')
+node5_name=$(echo ${node5_fullname} | awk -F ' ' '{print$2}')
 scp $node5_name:/users/gtc/varnishstat.log ./varnishstat-node5.log
 
-node6_name=$(echo ${node6_fullname} | awk -F ' ' '{print$1}')
+node6_name=$(echo ${node6_fullname} | awk -F ' ' '{print$2}')
 scp $node6_name:/users/gtc/varnishstat.log ./varnishstat-node6.log
 
